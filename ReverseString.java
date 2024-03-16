126f138f048cc1f662cda47f890550780d600b1f
mnikitha16 <139053515+mnikitha16@users.noreply.github.com>
2024-03-16 10:59:41 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,23 +1,23 @@
-  import java.util.Scanner;
-public class ReverseString
-{
-public static void main (String[] args[])
-{
-//define variables
-String instr;      //input String
-int inStrLen;      //length of the input String
-Scanner in=new Scanner(System.in);
-//prompt and read input as "String"
-System.out.print("Enter a String: ");
-inStr=in.next();
-intStrLen=inStr.length();
-System.out.print("The Reverse of the String "+ inStr + " is ");
-for(int CharIdx =intStrLen-1;CharIdx >=0;--CharIdx)
-{
-System.out.print(inStr.CharAt(CharIdx));
+import java.io.*;
+import java.util.Scanner;
+ 
+class GFG {
+    public static void main (String[] args) {
+       
+        String str= "Geeks", nstr="";
+        char ch;
+       
+      System.out.print("Original word: ");
+      System.out.println("Geeks"); //Example word
+       
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i); //extracts each character
+        nstr= ch+nstr; //adds each character in front of the existing string
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
 }
-}
-}
-
+ 
 
 	
